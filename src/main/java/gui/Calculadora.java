6e5ac03c68e27d3f2f138c5bb3b715ed3935c823
yohanCami <yosanchezm@unal.edu.co>
2023-11-06 17:14:27 -0500

    Calculadora Funcionando

--- src/main/java/gui/Calculadora.java
+++ src/main/java/gui/Calculadora.java
@@ -8,18 +8,18 @@ import javafx.stage.Stage;
 public class Calculadora extends Application  {
 	
 	public static void main(String args[]){ 
-	      ***(args); 
+	      launch(args); 
 	} 
 	
 	@Override
 	public void start(Stage window) throws Exception {
-		*** vb = new VBox();
-		window.setTitle(***);
+		VBox vb = new VBox();
+		window.setTitle("Calculadora");
 		
 		vb.getChildren().add(new Calculator());
-		Scene sc = new Scene(***, 270, 270);
-		window.setScene(***);
-		***.show();
+		Scene sc = new Scene(vb, 270, 270);
+		window.setScene(sc);
+		window.show();
 	}
 	
 }
--- src/main/java/gui/Calculator.java
+++ src/main/java/gui/Calculator.java
@@ -1,5 +1,8 @@
 package gui;
 
+import java.util.Arrays;
+import java.util.List;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -12,7 +15,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 
-public class Calculator extends VBox implements EventHandler<***>{
+public class Calculator extends VBox implements EventHandler<ActionEvent>{
 	
 	String number1 = "";
 	String number2 = "";
@@ -20,117 +23,163 @@ public class Calculator extends VBox implements EventHandler<***>{
 	Text displayText;
 	
 	public Calculator(){
-		super(10);
+		//super(10);
+		
 		this.displayText = new Text();
 		
 		Rectangle rt = new Rectangle(250, 50, Color.TRANSPARENT);
-		
 		rt.setStroke(Color.GRAY);
 		
 		StackPane sp =  new StackPane(rt, this.displayText);
 		
 		sp.setPadding(new Insets(10, 10, 10, 10));
 		
-		*** gd = new GridPane();
+		GridPane gd = new GridPane();
 		
-		gd.setPadding(new Insets(10, 10, 10, 10));
+		gd.setPadding(new Insets(20, 20, 20, 20));
 		gd.setVgap(5);
 		gd.setHgap(4);
 		
-		gd.setAlignment(Pos.***);
+		gd.setAlignment(Pos.CENTER);
 		
-		Button b7 = new ***("7");
-		gd.add(b7, 0, ***);
+		Button b7 = new Button("7");
+		gd.add(b7, 0, 0);
 		b7.setPrefWidth(50);
 		b7.setOnAction(this);
 		
-		*** b8 = new Button("8");
-		gd.add(b8, ***, 0);
+		Button b8 = new Button("8");
+		gd.add(b8, 1, 0);
 		b8.setPrefWidth(50);
 		b8.setOnAction(this);
 		
-		Button b9 = new Button(***);
-		gd.add(b9, 2, ***);
+		Button b9 = new Button("9");
+		gd.add(b9, 2, 0);
 		b9.setPrefWidth(50);
 		b9.setOnAction(this);
 		
 		Button div = new Button("/");
-		gd.add(div, ***, ***);
+		gd.add(div, 3, 0);
 		div.setPrefWidth(50);
-		div.setOnAction(***);
+		div.setOnAction(this);
 		
-		*** b4 = new Button("4");
+		Button b4 = new Button("4");
 		gd.add(b4, 0, 1);
 		b4.setPrefWidth(50);
 		b4.setOnAction(this);
 		
-		Button b5 = new ***("5");
-		gd.***(b5, 1, 1);
+		Button b5 = new Button("5");
+		gd.add(b5, 1, 1);
 		b5.setPrefWidth(50);
 		b5.setOnAction(this);
 		
 		Button b6 = new Button("6");
-		gd.add(b6, ***, ***);
+		gd.add(b6, 2, 1);
 		b6.setPrefWidth(50);
-		***.setOnAction(this);
+		b6.setOnAction(this);
 		
-		*** mul = new Button("*");
-		gd.add(mul, ***, 1);
+		Button mul = new Button("*");
+		gd.add(mul, 3, 1);
 		mul.setPrefWidth(50);
 		mul.setOnAction(this);
 		
-		Button b1 = new Button(***);
-		gd.add(b1, 0, ***);
+		Button b1 = new Button("1");
+		gd.add(b1, 0, 2);
 		b1.setPrefWidth(50);
 		b1.setOnAction(this);
 		
-		Button b2 = new ***("2");
-		gd.add(b2, ***, 2);
+		Button b2 = new Button("2");
+		gd.add(b2, 1, 2);
 		b2.setPrefWidth(50);
 		b2.setOnAction(this);
 		
-		Button b3 = new ***("3");
-		***.add(b3, 2, 2);
+		Button b3 = new Button("3");
+		gd.add(b3, 2, 2);
 		b3.setPrefWidth(50);
-		b3.setOnAction(***);
+		b3.setOnAction(this);
 		
-		*** minus = new Button("-");
-		gd.add(minus, 3, ***);
+		Button minus = new Button("-");
+		gd.add(minus, 3, 2);
 		minus.setPrefWidth(50);
 		minus.setOnAction(this);
 		
-		Button b0 = new ***("0");
+		Button b0 = new Button("0");
 		gd.add(b0, 0, 3, 2, 1);
 		b0.setPrefWidth(105);
-		***.setOnAction(this);
+		b0.setOnAction(this);
 		
 		Button plus = new Button("+");
-		gd.***(plus, ***, 3);
+		gd.add(plus, 2, 3);
 		plus.setPrefWidth(50);
 		plus.setOnAction(this);
 		
-		Button equals = new Button(***);
-		***.add(equals, 3, 3);
+		Button equals = new Button("=");
+		gd.add(equals, 3, 3);
 		equals.setPrefWidth(50);
 		equals.setOnAction(this);
 		
-		Button reset = new ***("C");
-		gd.add(reset, 0, 4, ***, 1);
+		Button reset = new Button("C");
+		gd.add(reset, 0, 4, 4, 1);
 		reset.setPrefWidth(215);
-		reset.setOnAction(***);
+		reset.setOnAction(this);
 		
 		this.getChildren().addAll(sp, gd);
 	}
 
 	@Override
-	public void ***(ActionEvent event) {
+	public void handle(ActionEvent event) {
 		
 		Button b = (Button) event.getSource();
 		String value = b.getText();
 		
-		***
-		***
-		***
+		switch (value) {
+        case "0": case "1": case "2": case "3": case "4":
+        case "5": case "6": case "7": case "8": case "9":
+
+            displayText.setText(displayText.getText() + value);
+            break;
+        
+        case "+": case "-": case "*": case "/":
+
+        	
+            operator = value;
+            number1 = displayText.getText();
+            displayText.setText(displayText.getText() + " " + value + " ");
+            break;
+
+        case "=":
+            
+        	String[] text = displayText.getText().split(" ");
+        	
+            number2 = text[text.length - 1];
+            int resultado = 0;
+
+            switch (operator) {
+                case "+":
+                    resultado = Integer.parseInt(number1) + Integer.parseInt(number2);
+                    break;
+                case "-":
+                    resultado = Integer.parseInt(number1) - Integer.parseInt(number2);
+                    break;
+                case "*":
+                    resultado = Integer.parseInt(number1) * Integer.parseInt(number2);
+                    break;
+                case "/":
+                    resultado = Integer.parseInt(number1) / Integer.parseInt(number2);
+                    break;
+            }
+
+            displayText.setText(Integer.toString(resultado));
+            break;
+
+        case "C":
+    
+            displayText.setText("");
+            number1 = "";
+            number2 = "";
+            operator = "";
+            break;
+		
+		}
 		
 	}
 	
